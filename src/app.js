e3cb71d7aa64dd1933492cf901282508c947b53d
chinann <chinan_19nd@hotmail.co.th>
2016-10-29 11:00:42 +0700

    react-router

---
+++ src/app.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import { SearchForm } from './search-form'
+import {
+    Router,
+    Route,
+    hashHistory,
+    Link,
+    IndexRoute
+} from 'react-router'
+
+const Home = () => (
+    <section>
+      
+        <h1> This is home </h1>
+    </section>
+)
+
+const Detail = () => (
+    <section>
+      
+        <h1> This is detail </h1>
+    </section>
+)
+
+const Nav = () => (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search</Link></li>
+            <li><Link to="/detail">Detail</Link></li>
+        </ul>
+    </nav>
+)
+
+const MovieList = (props) => (
+    <ul>
+    {props.movies.map((movie, i) => {
+        return (
+            <li key={i}>{movie.Title}</li>
+        )
+    })}
+    </ul>
+)
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: []
+        }
+    }
+    onSearch(query) {
+        axios.get(`http://www.omdbapi.com/?s=${query}&plot=short&r=json`)
+            .then(response => {
+                const movies = response.data.Search
+                this.setState({
+                    movies: movies
+                })
+            })
+    }
+    render() {
+        return (
+            <section>
+                <h1>Movie Collection</h1>
+                <SearchForm onSearchSubmit={this.onSearch.bind(this)} />
+                <MovieList movies={this.state.movies} />
+            </section>
+        )
+    }
+    
+}
+const App = props => (
+    <section>
+        <Nav />
+        {props.children}
+    </section>
+)
+
+class Main extends React.Component{
+    render(){
+        return(
+            <Router history= {hashHistory}>
+                <Route path="/" component={App} >
+                    <IndexRoute component={Home} />
+                    <Route path="search" component={Search} />
+                    <Route path="detail" component={Detail} />
+                </Route>
+            </Router>
+        )
+    }
+} 
+ReactDOM.render(<Main />, document.getElementById('app'))
